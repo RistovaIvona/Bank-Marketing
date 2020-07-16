55bcff33e0caa32959f58be2caa51608ce44285d
RistovaIvona <ivona.feit@gmail.com>
2020-07-16 17:42:26 +0200

    Template

---
+++ Bank-Marketing.py
@@ -0,0 +1,34 @@
+
+#Bank Client
+#Data Preparation
+
+#Dataset at first sight
+#YOUR CODE HERE
+#
+#
+#
+#
+
+
+#Descriptive Statistic
+#YOUR CODE HERE
+#
+#
+#
+#
+
+
+#Data quality check
+#YOUR CODE HERE
+#
+#
+#
+#
+
+#Build helpful visualization
+#YOUR CODE HERE
+#
+#
+#
+#
+ 
